d23a4489bf277e9d020f4d7142a6b5de1d10d5ff
danjac <danjac354@gmail.com>
2015-07-13 18:43:44 +0300

    Added pagination support

--- server/publish.js
+++ server/publish.js
@@ -2,6 +2,10 @@ Meteor.publish('allPosts', function() {
     return Posts.find();
 });
 
+Meteor.publish('postsByUser', function(userId) {
+    return Posts.find({ userId: userId });
+});
+
 Meteor.publish('singlePost', function(id) {
     return Posts.find(id);
 });
